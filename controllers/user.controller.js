ab7f93f4893e17c499ab6224b9ca425b638ea8cd
Erick Rios <paulrios@outlook.es>
2024-11-10 13:05:58 -0500

    docs: add description controller for user

---
+++ controllers/user.controller.js
@@ -0,0 +1,12 @@
+// Impementar la funcion getAllUsers
+// Implementar la funcion getUserById
+// Implementar la funcion updateUser
+// Implementar la funcion deleteUser
+// Buenas practicas a tomar en cuenta:
+// - Siempre usar try-catch para manejo de erroress
+// - cuando haya un error retornar codigo 500 (INTERNAL SERVER ERROR)
+//- Siempre retornar el codigo de estado correspondiente
+// 200 ->OK
+// 201 -> CREATED ()
+// 404 -> NOT FOUND (Cuando no se encuentra un recurso)
+// Si se animna usar loggers, siempre registrar un evento de error cada vez que ingresen al catch

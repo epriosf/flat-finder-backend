21226f637d210edcad11c1599643bcda4bf0f15f
Erick Rios <paulrios@outlook.es>
2024-11-20 18:07:11 -0500

    fix: change db connection string

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.USER_MONGO_DB_ATLAS}:${process.env.PASSWORD_MONGO_DB_ATLAS}@cluster0.gca1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    )
-    console.log('Conexión a la base de datos exitosa')
+      `mongodb+srv://${process.env.USER_MONGO_DB_ATLAS}:${process.env.PASSWORD_MONGO_DB_ATLAS}@cluster0.gca1u.mongodb.net/flatfinder-db?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log('Conexión a la base de datos exitosa');
   } catch (error) {
-    console.error('Error al conectar a la base de datos', error)
+    console.error('Error al conectar a la base de datos', error);
   }
-}
+};

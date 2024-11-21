e257bf15dc39122f7caa33ecdd8139455245af9c
Erick Rios <paulrios@outlook.es>
2024-11-20 20:05:33 -0500

    feat: add getFlatById, deleteFlat, updateFlat, saveFlat operations for flats

--- controllers/flat.controller.js
+++ controllers/flat.controller.js
@@ -45,4 +45,70 @@ const getFlats = async (req, res, next) => {
     next(error);
   }
 };
-export { getFlats };
+const getFlatById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const flat = await Flat.findById(id);
+
+    if (!flat) {
+      return res.status(404).json({ message: 'Flat not found' });
+    }
+
+    res.status(200).json(flat);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const saveFlat = async (req, res, next) => {
+  try {
+    const newFlat = new Flat(req.body);
+    await newFlat.save();
+    res
+      .status(201)
+      .json({ message: 'Flat created successfully', data: newFlat });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateFlat = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const flatData = req.body;
+
+    const updatedFlat = await Flat.findByIdAndUpdate(id, flatData, {
+      new: true,
+    });
+
+    if (!updatedFlat) {
+      return res.status(404).json({ message: 'Flat not found' });
+    }
+
+    res
+      .status(200)
+      .json({ message: 'Flat updated successfully', data: updatedFlat });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteFlat = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const deletedFlat = await Flat.findByIdAndDelete(id);
+
+    if (!deletedFlat) {
+      return res.status(404).json({ message: 'Flat not found' });
+    }
+
+    res
+      .status(200)
+      .json({ message: 'Flat deleted successfully', data: deletedFlat });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { deleteFlat, getFlatById, getFlats, saveFlat, updateFlat };
--- routes/flat.router.js
+++ routes/flat.router.js
@@ -1,9 +1,15 @@
-import express from 'express'
-import { getFlats } from './../controllers/flat.controller.js'
-const router = express.Router()
-router.get('/', getFlats)
-// router.get('', getFlatById)
-// router.post('', saveFlat)
-// router.put('', updateFlat)
-// router.delete('/:id', deleteFlat)
-export default router
+import express from 'express';
+import {
+  deleteFlat,
+  getFlatById,
+  getFlats,
+  saveFlat,
+  updateFlat,
+} from './../controllers/flat.controller.js';
+const router = express.Router();
+router.get('/', getFlats);
+router.get('/:id', getFlatById);
+router.post('', saveFlat);
+router.put('/:id', updateFlat);
+router.delete('/:id', deleteFlat);
+export default router;
